0810b902b4e219a77fe7b20b591b74407895fa4f
IshanParekh <ishanparekh31295@gmail.com>
2019-11-03 17:41:19 -0500

    modified local path

--- project.r
+++ project.r
@@ -1,4 +1,16 @@
+dev.off() # Clear the graph window
+cat('\014') # Clear the console
+rm(list=ls()) # Clear all user objects from the environment!!!
+
+
 library(jsonlite)
+#getwd
+#path = 'D:/Sem-1/IST-687/Project'
+#setwd(path)
+#dir()
 json_data <- fromJSON("data.json")
 json_data_frame <- as.data.frame(json_data)
 View(json_data_frame)
+
+
+
